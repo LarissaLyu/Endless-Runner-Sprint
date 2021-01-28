70b4b2e610feba5574b2bb54b390900e982fa5c2
LarissaLyu <61749917+LarissaLyu@users.noreply.github.com>
2021-01-28 16:16:52 +0800

    Player Movement Update
    Addedplayer vertical movment, and fixed the script of player.

--- Endless Runer/Assets/Scripts/Player.cs
+++ Endless Runer/Assets/Scripts/Player.cs
@@ -9,21 +9,18 @@ public class Player : MonoBehaviour
     public Rigidbody2D rb;
     public Text healthDisplay;
     public GameObject gameOver;
-    public GameObject effect;
+
+    public int health = 3;
+
+    public float moveSpeed;
+    Vector2 movement;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
     }
-    private Vector3 targetPos = new Vector3(0f,0f,-0.03f);
-    public float Xincrement;
 
-
-    public float speed;
-    public float maxWidth;
-    public float minWidth;
-
-    public int health = 3;
     // Update is called once per frame
     void Update()
     {
@@ -33,37 +30,15 @@ public class Player : MonoBehaviour
             gameOver.SetActive(true);
             Destroy(gameObject);
         }
-        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
-        if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < maxWidth)
-        {
-            Instantiate(effect, transform.position, Quaternion.identity);
-            targetPos = new Vector3(transform.position.x + Xincrement, transform.position.y, transform.position.z);
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > minWidth)
-        {
-            Instantiate(effect, transform.position, Quaternion.identity);
-            targetPos = new Vector3(transform.position.x - Xincrement, transform.position.y, transform.position.z);
-        }
-        //movement = new Vector2(Input.GetAxis("Horizontal"), transform.position.y);
 
-    }
-    /*private void FixedUpdate()
-    {
-        move(movement);
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        
     }
 
-    void move(Vector2 direction)
+    private void FixedUpdate()
     {
-        rb.AddForce(direction * speed);
-    }*/
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Block"))
-        {
-            Debug.Log("yes");
-            //transform.Translate(other.transform.position * other.gameObject.GetComponent<Block>().speed * Time.deltaTime, Space.Self);
-            //transform.position = other.transform.position;
-        }
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
     }
 
 }
